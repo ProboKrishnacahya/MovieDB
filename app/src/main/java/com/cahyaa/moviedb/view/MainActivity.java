01ade0277657a87310ad4166337a0f3baa3d1066
Probo Krishnacahya <prob.krshn@gmail.com>
2021-10-05 11:06:33 +0700

    Initial commit

--- app/src/main/java/com/cahyaa/moviedb/view/MainActivity.java
+++ app/src/main/java/com/cahyaa/moviedb/view/MainActivity.java
@@ -3,6 +3,7 @@ package com.cahyaa.moviedb.view;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
@@ -16,12 +17,14 @@ public class MainActivity extends AppCompatActivity {
 
     private MovieViewModel viewModel;
     private Button btn_hit;
+    private TextView txt_show;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        txt_show = findViewById(R.id.txt_show_main);
         viewModel = new ViewModelProvider(MainActivity.this).get(MovieViewModel.class);
 
         btn_hit = findViewById(R.id.btn_hit_main);
@@ -37,7 +40,8 @@ public class MainActivity extends AppCompatActivity {
     private Observer<Movies> showResultMovie = new Observer<Movies>() {
         @Override
         public void onChanged(Movies movies) {
-
+            String title = movies.getTitle();
+            txt_show.setText(title);
         }
     };
 
