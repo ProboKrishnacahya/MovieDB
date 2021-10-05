02905299e0b12d90bf062996e983529930f4551a
Probo Krishnacahya <prob.krshn@gmail.com>
2021-10-05 10:08:04 +0700

    Initial commit

---
+++ app/src/main/java/com/cahyaa/moviedb/retrofit/ApiEndPoint.java
@@ -0,0 +1,18 @@
+package com.cahyaa.moviedb.retrofit;
+
+import com.cahyaa.moviedb.model.Movies;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface ApiEndPoint {
+
+    @GET("movie/{movie_id}")
+    Call<Movies> getMovieById(
+            @Path("movie_id") String movieId,
+            @Query("api_key") String apiKey
+    );
+
+}
---
+++ app/src/main/java/com/cahyaa/moviedb/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.cahyaa.moviedb.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cahyaa.moviedb.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
