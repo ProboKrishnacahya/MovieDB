0c75bc16c292021aca0a9988bc7c8bc865d0c198
Probo Krishnacahya <prob.krshn@gmail.com>
2021-10-05 10:43:45 +0700

    Initial commit

--- app/src/main/java/com/cahyaa/moviedb/viewmodel/MovieViewModel.java
+++ app/src/main/java/com/cahyaa/moviedb/viewmodel/MovieViewModel.java
@@ -1,4 +1,33 @@
 package com.cahyaa.moviedb.viewmodel;
 
-public class MovieViewModel {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.cahyaa.moviedb.model.Movies;
+import com.cahyaa.moviedb.repositories.MovieRepository;
+
+public class MovieViewModel extends AndroidViewModel {
+
+    private MovieRepository repository;
+
+    public MovieViewModel(@NonNull Application application) {
+        super(application);
+        repository = MovieRepository.getInstance();
+    }
+
+    //*Begin of ViewModel getMovieById
+    private MutableLiveData<Movies> resultGetMovieById = new MutableLiveData<>();
+
+    public void getMovieById(String movieId) {
+        resultGetMovieById = repository.getMovieData(movieId);
+    }
+
+    public LiveData<Movies> getResultGetMovieById() {
+        return resultGetMovieById;
+    }
+    //End of ViewModel getMovieById
 }
