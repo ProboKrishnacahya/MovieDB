492520779eff0769b5c2669c4a6556d3ffbe4f99
Probo Krishnacahya <prob.krshn@gmail.com>
2021-10-05 11:41:16 +0700

    Initial commit

--- app/src/main/java/com/cahyaa/moviedb/helper/Const.java
+++ app/src/main/java/com/cahyaa/moviedb/helper/Const.java
@@ -3,6 +3,4 @@ package com.cahyaa.moviedb.helper;
 public class Const {
     public static final String API_KEY = "a9e08e43f36082a2181a2a0fcc1604d8";
     public static final String BASE_URL = "https://api.themoviedb.org/3/";
-
-
 }
--- app/src/main/java/com/cahyaa/moviedb/view/MainActivity.java
+++ app/src/main/java/com/cahyaa/moviedb/view/MainActivity.java
@@ -12,11 +12,13 @@ import androidx.lifecycle.ViewModelProvider;
 import com.cahyaa.moviedb.R;
 import com.cahyaa.moviedb.model.Movies;
 import com.cahyaa.moviedb.viewmodel.MovieViewModel;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private MovieViewModel viewModel;
     private Button btn_hit;
+    private TextInputLayout til_movie_id;
     private TextView txt_show;
 
     @Override
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        til_movie_id = findViewById(R.id.til_movie_id_main);
         txt_show = findViewById(R.id.txt_show_main);
         viewModel = new ViewModelProvider(MainActivity.this).get(MovieViewModel.class);
 
@@ -31,7 +34,13 @@ public class MainActivity extends AppCompatActivity {
         btn_hit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                viewModel.getMovieById("350");
+                String movieId = til_movie_id.getEditText().getText().toString().trim();
+                if (movieId.isEmpty()) {
+                    til_movie_id.setError("Please fill Movie ID Field!");
+                } else {
+                    til_movie_id.setError(null);
+                }
+                viewModel.getMovieById(movieId);
                 viewModel.getResultGetMovieById().observe(MainActivity.this, showResultMovie);
             }
         });
@@ -40,6 +49,12 @@ public class MainActivity extends AppCompatActivity {
     private Observer<Movies> showResultMovie = new Observer<Movies>() {
         @Override
         public void onChanged(Movies movies) {
+            if (movies == null) {
+                txt_show.setText("Movie ID is not available in MovieDb");
+            } else {
+                String title = movies.getTitle();
+                txt_show.setText(title);
+            }
             String title = movies.getTitle();
             txt_show.setText(title);
         }
